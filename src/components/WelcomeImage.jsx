5f45ac6e89269e2a5f7d113a5918fc44b8da369a
Peter Ivey-Hansen <peteriveyhansen@gmail.com>
2020-06-07 23:24:31 +0200

    adding better logic to the floating blobs

--- src/components/WelcomeImage.jsx
+++ src/components/WelcomeImage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import Typewriter from 'typewriter-effect';
 import { BGC_RED } from '../assets/images/index';
@@ -8,57 +8,58 @@ const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i);
 
 const randomWH = () => {
-  const rangeLimits = range(12, 14);
+  const rangeLimits = range(12.5, 15.5);
   const randomIndex = Math.ceil(Math.random() * rangeLimits.length);
   return rangeLimits[randomIndex];
 };
 
-const blob1Transform = keyframes`
-  0%,
-  100% {
-    border-radius: 55% 45% 57% 43% / 60% 36% 64% 40%;
-    width: 15rem;
-    height: 15rem;
-  }
-  25% {
-    border-radius: 75% 25% 42% 58% / 62% 31% 69% 38%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-  50% {
-    border-radius: 44% 56% 29% 71% / 72% 62% 38% 28%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-  75% {
-    border-radius: 30% 70% 36% 64% / 69% 52% 48% 31%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-`;
-const blob2Transform = keyframes`
-  0%,
-  100% {
-    border-radius: 66% 34% 62% 38% / 40% 66% 34% 60%;
-    width: 15rem;
-    height: 15rem;
-  }
-  25% {
-    border-radius: 58% 42% 44% 56% / 53% 57% 43% 47%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-  50% {
-    border-radius: 43% 57% 37% 63% / 65% 42% 58% 35%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-  75% {
-    border-radius: 66% 34% 60% 40% / 49% 33% 67% 51%;
-    width: ${randomWH()}rem;
-    height: ${randomWH()}rem;
-  }
-`;
+const blob1Transform = coordinates =>
+  keyframes`
+    0%,
+    100% {
+      border-radius: 55% 45% 57% 43% / 60% 36% 64% 40%;
+      transform: translate(${coordinates[0]}px, ${coordinates[1]}px);
+    }
+    25% {
+      border-radius: 75% 25% 42% 58% / 62% 31% 69% 38%;
+      width: ${randomWH()}rem;
+      height: ${randomWH()}rem;
+    }
+    50% {
+      transform: translate(${coordinates[2]}px, ${coordinates[3]}px);
+      border-radius: 44% 56% 29% 71% / 72% 62% 38% 28%;
+      width: ${randomWH()}rem;
+      height: ${randomWH()}rem;
+    }
+    75% {
+      border-radius: 30% 70% 36% 64% / 69% 52% 48% 31%;
+      width: ${randomWH()}rem;
+      height: ${randomWH()}rem;
+    }`;
+
+// const blob2Transform = keyframes`
+//   0%,
+//   100% {
+//     border-radius: 66% 34% 62% 38% / 40% 66% 34% 60%;
+//     width: 15rem;
+//     height: 15rem;
+//   }
+//   25% {
+//     border-radius: 58% 42% 44% 56% / 53% 57% 43% 47%;
+//     width: ${randomWH()}rem;
+//     height: ${randomWH()}rem;
+//   }
+//   50% {
+//     border-radius: 43% 57% 37% 63% / 65% 42% 58% 35%;
+//     width: ${randomWH()}rem;
+//     height: ${randomWH()}rem;
+//   }
+//   75% {
+//     border-radius: 66% 34% 60% 40% / 49% 33% 67% 51%;
+//     width: ${randomWH()}rem;
+//     height: ${randomWH()}rem;
+//   }
+// `;
 
 const WelcomeArea = styled.div`
   display: flex;
@@ -74,15 +75,17 @@ const WelcomeArea = styled.div`
 
 const Blob1 = styled.div`
   background: #0a8754;
+  width: 15rem;
+  height: 15rem;
   z-index: 2;
-  animation: ${blob1Transform} 8s ease-in-out infinite;
+  animation: ${({ coords }) => blob1Transform(coords)} 10s ease-in-out infinite;
 `;
 
-const Blob2 = styled.div`
-  background: #07393c;
-  z-index: 2;
-  animation: ${blob2Transform} 8s ease-in-out infinite;
-`;
+// const Blob2 = styled.div`
+//   background: #07393c;
+//   z-index: 2;
+//   animation: ${blob2Transform} 8s ease-in-out infinite;
+// `;
 
 const WelcomeText = styled.div`
   width: auto;
@@ -134,47 +137,53 @@ const Title = styled.h2`
 `;
 
 const WelcomeImage = () => {
-  const blob1Ref = useRef(null);
-  const blob2Ref = useRef(null);
-  const welcomeAreaRef = useRef(null);
-  let parentWidth = null;
-  let parentHeight = null;
+  const [blobCoordinates, setBlobCoordinates] = useState([]);
+  const [previousPos, setPreviousPos] = useState([]);
+  let count = 0;
 
   useEffect(() => {
-    if (welcomeAreaRef.current && blob1Ref.current && blob2Ref.current) {
-      parentWidth = welcomeAreaRef.current.clientWidth;
-      parentHeight = welcomeAreaRef.current.clientWidth;
-      console.log(welcomeAreaRef);
-      const makeNewPosition = () => {
-        // Get viewport dimensions (remove the dimension of the div)
-        const h = parentHeight - 16 * 15;
-        const w = parentWidth - 16 * 15;
+    const {
+      width: welcomeAreaWidth,
+      height: welcomeAreaHeight,
+    } = document.getElementById('welcomeArea').getBoundingClientRect();
+    const parentWidth = welcomeAreaWidth;
+    const parentHeight = welcomeAreaHeight;
+    const makeNewPosition = () => {
+      const h = parentHeight - 16 * 14.25;
+      const w = parentWidth - 16 * 14.25;
+      const nh = Math.floor(Math.random() * h);
+      const nw = Math.floor(Math.random() * w);
 
-        const nh = Math.floor(Math.random() * h);
-        const nw = Math.floor(Math.random() * w);
+      return [nh, nw];
+    };
 
-        return [nh, nw];
-      };
+    /*
+      you cannot restart the animation so instead try preloading an array with say 10 - 20
+      position points and then when the last point is reached reverse the array and cycle back
+      through the points
+    */
 
-      const moveBlob = () => {
-        var newPos1 = makeNewPosition();
-        var newPos2 = makeNewPosition();
-        blob1Ref.current.style.transform = `translate(${newPos1[0]}px, ${
-          newPos1[1]
-        }px)`;
-        blob2Ref.current.style.transform = `translate(${newPos2[0]}px, ${
-          newPos2[1]
-        }px)`;
-      };
-      setInterval(() => {
-        moveBlob();
-      }, 2000);
-    }
+    const moveBlob = () => {
+      const newPos1 = makeNewPosition();
+      const newPos2 = makeNewPosition();
+      setPreviousPos(newPos2);
+      let positions = [];
+      if (count < 1) {
+        positions = newPos1.concat(newPos2);
+        count = 1;
+        return positions;
+      } else {
+        positions = previousPos.concat(newPos1);
+        return positions;
+      }
+    };
+
+    setBlobCoordinates(moveBlob());
   }, []);
+
   return (
-    <WelcomeArea ref={welcomeAreaRef}>
-      <Blob1 ref={blob1Ref} />
-      <Blob2 ref={blob2Ref} />
+    <WelcomeArea id="welcomeArea">
+      {blobCoordinates.length > 0 && <Blob1 coords={blobCoordinates} />}
       <Title>
         <div>Peter</div>
         <div>Ivey</div>
