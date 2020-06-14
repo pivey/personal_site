83bb9de06185c77abaabc791e51a66e8a3366a56
Peter Ivey-Hansen <peteriveyhansen@gmail.com>
2020-06-14 23:33:13 +0200

    fix: improve blobs and blob animations

--- src/components/WelcomeImage.jsx
+++ src/components/WelcomeImage.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
+import Anime, { anime } from 'react-anime';
 import styled, { keyframes } from 'styled-components';
 import Typewriter from 'typewriter-effect';
 import { BGC_RED } from '../assets/images/index';
@@ -13,53 +14,48 @@ const randomWH = () => {
   return rangeLimits[randomIndex];
 };
 
-const blob1Transform = coordinates =>
-  keyframes`
-    0%,
-    100% {
-      border-radius: 55% 45% 57% 43% / 60% 36% 64% 40%;
-      transform: translate(${coordinates[0]}px, ${coordinates[1]}px);
-    }
-    25% {
-      border-radius: 75% 25% 42% 58% / 62% 31% 69% 38%;
-      width: ${randomWH()}rem;
-      height: ${randomWH()}rem;
-    }
-    50% {
-      transform: translate(${coordinates[2]}px, ${coordinates[3]}px);
-      border-radius: 44% 56% 29% 71% / 72% 62% 38% 28%;
-      width: ${randomWH()}rem;
-      height: ${randomWH()}rem;
-    }
-    75% {
-      border-radius: 30% 70% 36% 64% / 69% 52% 48% 31%;
-      width: ${randomWH()}rem;
-      height: ${randomWH()}rem;
-    }`;
+const blob1Transform = keyframes`
+      0%,
+      100% {
+        border-radius: 55% 45% 57% 43% / 60% 36% 64% 40%;
+      }
+      25% {
+        border-radius: 75% 25% 42% 58% / 62% 31% 69% 38%;
+        width: ${randomWH()}rem;
+        height: ${randomWH()}rem;
+      }
+      50% {
+        border-radius: 44% 56% 29% 71% / 72% 62% 38% 28%;
+        width: ${randomWH()}rem;
+        height: ${randomWH()}rem;
+      }
+      75% {
+        border-radius: 30% 70% 36% 64% / 69% 52% 48% 31%;
+        width: ${randomWH()}rem;
+        height: ${randomWH()}rem;
+      }`;
 
-// const blob2Transform = keyframes`
-//   0%,
-//   100% {
-//     border-radius: 66% 34% 62% 38% / 40% 66% 34% 60%;
-//     width: 15rem;
-//     height: 15rem;
-//   }
-//   25% {
-//     border-radius: 58% 42% 44% 56% / 53% 57% 43% 47%;
-//     width: ${randomWH()}rem;
-//     height: ${randomWH()}rem;
-//   }
-//   50% {
-//     border-radius: 43% 57% 37% 63% / 65% 42% 58% 35%;
-//     width: ${randomWH()}rem;
-//     height: ${randomWH()}rem;
-//   }
-//   75% {
-//     border-radius: 66% 34% 60% 40% / 49% 33% 67% 51%;
-//     width: ${randomWH()}rem;
-//     height: ${randomWH()}rem;
-//   }
-// `;
+const blob2Transform = keyframes`
+  0%,
+  100% {
+    border-radius: 66% 34% 62% 38% / 40% 66% 34% 60%;
+  }
+  25% {
+    border-radius: 58% 42% 44% 56% / 53% 57% 43% 47%;
+    width: ${randomWH()}rem;
+    height: ${randomWH()}rem;
+  }
+  50% {
+    border-radius: 43% 57% 37% 63% / 65% 42% 58% 35%;
+    width: ${randomWH()}rem;
+    height: ${randomWH()}rem;
+  }
+  75% {
+    border-radius: 66% 34% 60% 40% / 49% 33% 67% 51%;
+    width: ${randomWH()}rem;
+    height: ${randomWH()}rem;
+  }
+`;
 
 const WelcomeArea = styled.div`
   display: flex;
@@ -73,19 +69,14 @@ const WelcomeArea = styled.div`
   overflow: hidden;
 `;
 
-const Blob1 = styled.div`
-  background: #0a8754;
-  width: 15rem;
-  height: 15rem;
-  z-index: 2;
-  animation: ${({ coords }) => blob1Transform(coords)} 10s ease-in-out infinite;
-`;
-
-// const Blob2 = styled.div`
-//   background: #07393c;
-//   z-index: 2;
-//   animation: ${blob2Transform} 8s ease-in-out infinite;
-// `;
+const Blob = ({ bgc, animationName, duration }) =>
+  styled.div`
+    background: ${bgc};
+    width: 15rem;
+    height: 15rem;
+    z-index: 2;
+    animation: ${animationName} ${duration} ease-in-out infinite;
+  `;
 
 const WelcomeText = styled.div`
   width: auto;
@@ -137,9 +128,8 @@ const Title = styled.h2`
 `;
 
 const WelcomeImage = () => {
-  const [blobCoordinates, setBlobCoordinates] = useState([]);
-  const [previousPos, setPreviousPos] = useState([]);
-  let count = 0;
+  const [blob1Coordinates, setBlob1Coordinates] = useState([]);
+  const [blob2Coordinates, setBlob2Coordinates] = useState([]);
 
   useEffect(() => {
     const {
@@ -149,41 +139,70 @@ const WelcomeImage = () => {
     const parentWidth = welcomeAreaWidth;
     const parentHeight = welcomeAreaHeight;
     const makeNewPosition = () => {
-      const h = parentHeight - 16 * 14.25;
-      const w = parentWidth - 16 * 14.25;
+      const blobDimensions = 16 * 15;
+      const h = parentHeight - blobDimensions;
+      const w = parentWidth - blobDimensions;
       const nh = Math.floor(Math.random() * h);
       const nw = Math.floor(Math.random() * w);
 
       return [nh, nw];
     };
 
-    /*
-      you cannot restart the animation so instead try preloading an array with say 10 - 20
-      position points and then when the last point is reached reverse the array and cycle back
-      through the points
-    */
-
-    const moveBlob = () => {
-      const newPos1 = makeNewPosition();
-      const newPos2 = makeNewPosition();
-      setPreviousPos(newPos2);
+    const createCoordinates = () => {
       let positions = [];
-      if (count < 1) {
-        positions = newPos1.concat(newPos2);
-        count = 1;
-        return positions;
-      } else {
-        positions = previousPos.concat(newPos1);
-        return positions;
+      for (let i = 1; i < 30; i++) {
+        const coordinates = makeNewPosition();
+        positions.push(coordinates);
       }
+      return positions;
     };
 
-    setBlobCoordinates(moveBlob());
+    setBlob1Coordinates(createCoordinates());
+    setBlob2Coordinates(createCoordinates());
   }, []);
+
+  const blob1Coords = blob1Coordinates.map(coords => ({
+    translateX: coords[0],
+    translateY: coords[1],
+    duration: Math.ceil(Math.random() * 5000) + 3500,
+  }));
+  const blob2Coords = blob2Coordinates.map(coords => ({
+    translateX: coords[0],
+    translateY: coords[1],
+    duration: Math.ceil(Math.random() * 5000) + 3500,
+  }));
+
+  const animationProps = coordinates => ({
+    autoplay: true,
+    keyframes: [
+      { opacity: [0, 1], duration: 3000 },
+      ...coordinates,
+      { opacity: [1, 0], duration: 3000 },
+    ],
+    easing: 'easeInOutCubic',
+    loop: true,
+  });
+
+  const Blob1 = Blob({
+    bgc: '#543864',
+    animationName: blob1Transform,
+    duration: '8s',
+  });
+
+  const Blob2 = Blob({
+    bgc: '#12947f',
+    animationName: blob2Transform,
+    duration: '8s',
+  });
 
   return (
     <WelcomeArea id="welcomeArea">
-      {blobCoordinates.length > 0 && <Blob1 coords={blobCoordinates} />}
+      <Anime {...animationProps(blob1Coords)}>
+        <Blob1 />
+      </Anime>
+      <Anime {...animationProps(blob2Coords)}>
+        <Blob2 />
+      </Anime>
       <Title>
         <div>Peter</div>
         <div>Ivey</div>
@@ -215,7 +234,7 @@ const WelcomeImage = () => {
               .typeString('Firebase | GraphQL')
               .pauseFor(1500)
               .deleteAll()
-              .typeString('Flutter')
+              .typeString('Typescript')
               .pauseFor(1500)
               .deleteAll()
               .typeString('Stockholm, Sweden')
